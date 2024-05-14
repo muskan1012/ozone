c8e38002499afec71fd08140cd08341dd9ac91c6
muskan1012 <muskan1012mishra@gmail.com>
2024-05-14 16:43:28 +0530

    testing [skip ci]

--- hadoop-ozone/integration-test/src/test/java/org/apache/hadoop/ozone/client/rpc/TestOzoneRpcClient.java
+++ hadoop-ozone/integration-test/src/test/java/org/apache/hadoop/ozone/client/rpc/TestOzoneRpcClient.java
@@ -1,61 +1,61 @@
-/**
- * Licensed to the Apache Software Foundation (ASF) under one
- * or more contributor license agreements.  See the NOTICE file
- * distributed with this work for additional information
- * regarding copyright ownership.  The ASF licenses this file
- * to you under the Apache License, Version 2.0 (the
- * "License"); you may not use this file except in compliance
- * with the License.  You may obtain a copy of the License at
- * <p>
- * http://www.apache.org/licenses/LICENSE-2.0
- * <p>
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-
-package org.apache.hadoop.ozone.client.rpc;
-
-import java.io.IOException;
-import org.apache.hadoop.hdds.conf.OzoneConfiguration;
-import org.apache.hadoop.hdds.scm.ScmConfigKeys;
-
-import org.apache.hadoop.ozone.OzoneConfigKeys;
-import org.junit.jupiter.api.AfterAll;
-import org.junit.jupiter.api.BeforeAll;
-import org.junit.jupiter.api.Timeout;
-
-
-/**
- * This class is to test all the public facing APIs of Ozone Client.
- */
-@Timeout(300)
-public class TestOzoneRpcClient extends TestOzoneRpcClientAbstract {
-
   /**
-   * Create a MiniOzoneCluster for testing.
+   * Licensed to the Apache Software Foundation (ASF) under one
+   * or more contributor license agreements.  See the NOTICE file
+   * distributed with this work for additional information
+   * regarding copyright ownership.  The ASF licenses this file
+   * to you under the Apache License, Version 2.0 (the
+   * "License"); you may not use this file except in compliance
+   * with the License.  You may obtain a copy of the License at
    * <p>
-   * Ozone is made active by setting OZONE_ENABLED = true
-   *
-   * @throws IOException
+   * http://www.apache.org/licenses/LICENSE-2.0
+   * <p>
+   * Unless required by applicable law or agreed to in writing, software
+   * distributed under the License is distributed on an "AS IS" BASIS,
+   * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+   * See the License for the specific language governing permissions and
+   * limitations under the License.
    */
-  @BeforeAll
-  public static void init() throws Exception {
-    OzoneConfiguration conf = new OzoneConfiguration();
-    conf.setInt(ScmConfigKeys.OZONE_SCM_PIPELINE_OWNER_CONTAINER_COUNT, 1);
-    conf.setBoolean(OzoneConfigKeys.OZONE_ACL_ENABLED, true);
-    conf.set(OzoneConfigKeys.OZONE_ACL_AUTHORIZER_CLASS,
-        OzoneConfigKeys.OZONE_ACL_AUTHORIZER_CLASS_NATIVE);
-    startCluster(conf);
-  }
+
+  package org.apache.hadoop.ozone.client.rpc;
+
+  import java.io.IOException;
+  import org.apache.hadoop.hdds.conf.OzoneConfiguration;
+  import org.apache.hadoop.hdds.scm.ScmConfigKeys;
+
+  import org.apache.hadoop.ozone.OzoneConfigKeys;
+  import org.junit.jupiter.api.AfterAll;
+  import org.junit.jupiter.api.BeforeAll;
+  import org.junit.jupiter.api.Timeout;
+
 
   /**
-   * Close OzoneClient and shutdown MiniOzoneCluster.
+   * This class is to test all the public facing APIs of Ozone Client.
    */
-  @AfterAll
-  public static void shutdown() throws IOException {
-    shutdownCluster();
+  @Timeout(300)
+  public class TestOzoneRpcClient extends TestOzoneRpcClientAbstract {
+
+    /**
+     * Create a MiniOzoneCluster for testing.
+     * <p>
+     * Ozone is made active by setting OZONE_ENABLED = true
+     *
+     * @throws IOException
+     */
+    @BeforeAll
+    public static void init() throws Exception {
+      OzoneConfiguration conf = new OzoneConfiguration();
+      conf.setInt(ScmConfigKeys.OZONE_SCM_PIPELINE_OWNER_CONTAINER_COUNT, 1);
+      conf.setBoolean(OzoneConfigKeys.OZONE_ACL_ENABLED, true);
+      conf.set(OzoneConfigKeys.OZONE_ACL_AUTHORIZER_CLASS,
+          OzoneConfigKeys.OZONE_ACL_AUTHORIZER_CLASS_NATIVE);
+      startCluster(conf);
+    }
+
+    /**
+     * Close OzoneClient and shutdown MiniOzoneCluster.
+     */
+    @AfterAll
+    public static void shutdown() throws IOException {
+      shutdownCluster();
+    }
   }
-}
